9b56df3bc5647fc20a794962e38814d17406da81
betNevS <1173325467@qq.com>
2022-05-08 15:36:47 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/betNevS/instrument_trace
+
+go 1.18
---
+++ trace.go
@@ -0,0 +1,63 @@
+package trace
+
+import (
+	"bytes"
+	"fmt"
+	"runtime"
+	"strconv"
+	"sync"
+)
+
+var (
+	mu             sync.Mutex
+	m              = make(map[uint64]int)
+	goroutineSpace = []byte("goroutine ")
+)
+
+func Trace() func() {
+	pc, _, _, ok := runtime.Caller(1)
+	if !ok {
+		panic("not found caller")
+	}
+
+	fn := runtime.FuncForPC(pc)
+	name := fn.Name()
+	gid := curGoroutineID()
+
+	mu.Lock()
+	indents := m[gid]
+	m[gid] = indents + 1
+	mu.Unlock()
+	printTrace(gid, name, "->", indents+1)
+	return func() {
+		mu.Lock()
+		indents = m[gid]
+		m[gid] = indents - 1
+		mu.Unlock()
+		printTrace(gid, name, "<-", indents)
+	}
+}
+
+func printTrace(id uint64, name, arrow string, indent int) {
+	indents := ""
+	for i := 0; i < indent; i++ {
+		indents += "    "
+	}
+	fmt.Printf("g[%05d]:%s%s%s\n", id, indents, arrow, name)
+}
+
+func curGoroutineID() uint64 {
+	b := make([]byte, 64)
+	b = b[:runtime.Stack(b, false)]
+	b = bytes.TrimPrefix(b, goroutineSpace)
+	i := bytes.IndexByte(b, ' ')
+	if i < 0 {
+		panic(fmt.Sprintf("No space in %q", b))
+	}
+	b = b[:i]
+	n, err := strconv.ParseUint(string(b), 10, 64)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to parse goroutine ID out of %q: %v", b, err))
+	}
+	return n
+}
